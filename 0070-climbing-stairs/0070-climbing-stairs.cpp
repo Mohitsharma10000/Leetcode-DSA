688a3834f336bec10d29d28030dcffbd7e1df9fb
Mohit Sharma <ms1642404@gmail.com>
2026-08-19 16:22:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.09%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int count(int n,vector<int>&dp){
+    if(n<=2) return n;
+    if(dp[n]!=-1) return dp[n];
+     dp[n]=count(n-1,dp)+count(n-2,dp);
+     return dp[n];
+    
+
+}
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return count(n,dp);
+        
+    }
+};
