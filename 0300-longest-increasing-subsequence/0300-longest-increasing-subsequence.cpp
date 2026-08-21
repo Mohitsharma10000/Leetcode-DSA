4bdb47dcda25bb596f503a29663310c79f8f9424
Mohit Sharma <ms1642404@gmail.com>
2026-08-21 15:44:00 +0530

    Time: 584 ms (10.67%), Space: 296.6 MB (13.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,11 +4,12 @@ int solve(vector<int>&nums,int i,int previous,vector<vector<int>>&dp){
     if(i==nums.size()) return 0;
     if(dp[i][previous+1]!=-1) return dp[i][previous+1];
     
-    int notpick=solve(nums,i+1,previous,dp);
+    
     int pick=0;
     if(previous==-1||nums[i]>nums[previous]){
         pick=1+solve(nums,i+1,i,dp);
     }
+    int notpick=solve(nums,i+1,previous,dp);
     dp[i][previous+1]=max(pick,notpick);
     return dp[i][previous+1];
 }
