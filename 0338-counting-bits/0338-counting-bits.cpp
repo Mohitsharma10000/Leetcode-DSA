ea5a76a215fa5291389abe4c586b328d157c1a60
Mohit Sharma <ms1642404@gmail.com>
2026-08-24 12:48:01 +0530

    Time: 3 ms (24.67%), Space: 11.3 MB (28.88%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-void binary(vector<int>&arr,int n,vector<int>&ans){
-    int count=0;
-    while(n>0){
-        int t=n%2;
-        n=n/2;
-        if(t==1){
-            count++;
+    void binary( int n, vector<int>& ans) {
+        int count = 0;
+        while (n > 0) {
+            int t = n % 2;
+            n = n / 2;
+            if (t == 1) {
+                count++;
+            }
         }
+        ans.push_back(count);
     }
-    ans.push_back(count);
-
-}
     vector<int> countBits(int n) {
-        vector<int>arr;
-        for(int i=0;i<=n;i++){
-            arr.push_back(i);
-        }
-        vector<int>ans;
-        for(int i=0;i<=n;i++)
-        binary(arr,i,ans);
+        vector<int> ans;
+        for (int i = 0; i <= n; i++)
+            binary(i, ans);
         return ans;
-        
     }
 };
