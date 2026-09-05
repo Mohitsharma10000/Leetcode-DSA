233252961cf198553b12dc1d19325e894bb0cb9d
Mohit Sharma <ms1642404@gmail.com>
2026-09-05 23:46:58 +0530

    Time: 23 ms (15.6%), Space: 10.7 MB (7.8%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     int balancedString(string s) {
-        int n = s.size();
-        int target = n / 4;
-        unordered_map<char, int> freq;
-        
-       
-        for (char c : s) freq[c]++;
-        
-      
-        bool balanced = true;
-        for (char c : {'Q','W','E','R'}) {
-            if (freq[c] > target) balanced = false;
+        int n=s.length();
+        int target=n/4,minlen=n;
+        unordered_map<char,int>freq;
+        for(char ch:s){
+            freq[ch]++;
         }
-        if (balanced) return 0;
-        
-        int left = 0, minLen = n;
-        
-        for (int right = 0; right < n; right++) {
-            freq[s[right]]--;   
-
-            while (left < n && freq['Q'] <= target && freq['W'] <= target && 
-                   freq['E'] <= target && freq['R'] <= target) {
-                minLen = min(minLen, right - left + 1);
-                freq[s[left]]++;   
-                left++;
+        bool balance=true;
+        for(char c:{'Q','W','E','R'}){
+            if(freq[c]>target) balance=false;
+        }
+        if(balance) return 0;
+        int left=0;
+        for(int right=0;right<n;right++){
+            freq[s[right]]--;
+            while(left<n&&freq['Q']<=target&&freq['W']<=target&&freq['E']<=target&&freq['R']<=target){
+                minlen=min(minlen,right-left+1);
+                freq[s[left++]]++;
             }
+
+
+
         }
+        return minlen;
         
-        return minLen;
     }
 };
