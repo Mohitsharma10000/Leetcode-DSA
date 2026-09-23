afc1db63ae6bb581f5fb22447827303f58ea19c5
Mohit Sharma <ms1642404@gmail.com>
2026-09-23 23:49:29 +0530

    Time: 0 ms (100%), Space: 8 MB (79.36%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length();
+        int upper = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(isupper(word[i])) {
+                upper++;
+            }
+        }
+
+        if(upper == 0 || upper == n)
+            return true;
+
+        if(upper == 1 && isupper(word[0]))
+            return true;
+
+        return false;
+    }
+};
